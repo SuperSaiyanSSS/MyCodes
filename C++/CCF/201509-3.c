e0c6bf6d31d56b871c0d5ea095e4307183abcb19
SuperSaiyanSSS <2304582602@qq.com>
2017-09-30 10:29:40 +0800

    无法AC。。不清楚哪里有问题

--- C++/CCF/201509-3.c
+++ C++/CCF/201509-3.c
@@ -24,21 +24,24 @@ int main(int argc, char *argv[]) {
 	int find_flag = 0;
 	int next_flag = 1; 
 	
-
-
-	
-	
 	scanf("%d %d", &m, &n);
 	//清空scanf的缓存区！ 
 	fflush(stdin);
 	int i, j, k;
 	for(i=0;i<m;i++){
-		gets(moban[i]);
+		scanf("%[^\n]", &moban[i]); 
+		fflush(stdin);
+//		printf("%s ------- %d\n", moban[i], i); 
+	//	gets(moban[i]);
 	}
 	for(i=0;i<n;i++){
 		scanf("%s ",&name[i]);
-		gets(value[i]);
+	//	gets(value[i]);
+		scanf("%[^\n]", &value[i]);
+	//	printf("name[%d]----%s----%s\n", i, name[i], value[i]);
 	}
+	
+	
 	
 	while(next_flag!=0){
 		next_flag = 0;
@@ -92,64 +95,45 @@ int main(int argc, char *argv[]) {
 		while(fnode!=NULL){
 			int nowhang = fnode->hang;
 			int nownamehang;
+			
 			for(i=0;i<n;i++){
-				if(strcmp(name[i], fnode->s, fnode->s_count)==0){
+
+				if(strncmp(name[i], fnode->s, fnode->s_count)==0){
 					nownamehang = i;
-					printf("name[i] %s ----- %s\n", name[i], fnode->s);
 					break;
 				}
+
 			}
+			if(i==n){
+				char front[200];
+				strncpy(front, moban[nowhang], fnode->i1);
+				strncpy(front+fnode->i1, moban[nowhang]+fnode->i4+3, strlen(moban[nowhang])-fnode->i4-2);
+				strcpy(moban[nowhang], front);
+				fnode = fnode->next;
+				continue;	
+			}
+			
 			char front[200];
 			//三段拼凑 
 			strncpy(front, moban[nowhang], fnode->i1);
-//			for(i=0;i<fnode->i1;i++)
-//			printf("%c", front[i]);
-//			printf("\n");
+
 			// +1 -2是因为去除两个双引号 
 			strncpy(front+fnode->i1, value[nownamehang]+1, strlen(value[nownamehang])-2);  
-//			for(i=0;i<fnode->i1+strlen(value[nownamehang])-2;i++)
-//			printf("%c", front[i]);
-//			printf("\n");
+
 			//+3 是因为跨过空格/}/} 
 			strncpy(front+(fnode->i1+ strlen(value[nownamehang])-2), moban[nowhang]+fnode->i4+3, strlen(moban[nowhang])-fnode->i4-2);
-//			for(i=0;i<fnode->i1+strlen(value[nownamehang])-2+strlen(moban[nowhang])-fnode->i4-2;i++)
-//			printf("%c", front[i]);
-//			printf("\n");
+
 			
 			strcpy(moban[nowhang], front);
-			free(fnode);
 			fnode = fnode->next;
 		}
 	}
-//	printf("--------------------\n");
-	for(i=0;i<m;i++){
-		puts(moban[i]);
+	for(i=0;i<m-1;i++){
+		printf("%s", moban[i]);
+		printf("\n");
 	}
-	
-	
-//	int c = fnode->s_count;
-//	for(i=0;i<c;i++){
-//	printf("%c ", fnode->s[i]);
-//}
-//	printf("\n%d %d %d %d %d ", fnode->i1, fnode->i2, fnode->i3, fnode->i4, fnode->hang);
-	
-	
-//	
-//	char c[100][100];
-//	int i;
-//	for(i=0;i<4;i++){
-//		
-//	gets(c[i]);
-//}
-//
-////////	printf("%s", c);
-//////	c[0] = 'a';
-//////		c[1] = ' ';
-//////	c[2] = 'b';
-//////	
-//////	printf("%d\n", strlen(c));
-//	printf("2%s2", c[2]);
-////	
+	printf("%s", moban[m-1]);
+
 
 	return 0;
 }
